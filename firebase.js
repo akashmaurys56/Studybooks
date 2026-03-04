4f85a3b4f8949ab10374ca1bcee78b9a308f4e2d
akashmaurys56 <mauryaumesh195@gmail.com>
2026-03-04 19:14:22 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAW7iKFkof_hGxXtBNS8SA3VMfVM3O6QA0",
+  authDomain: "studybooks-423b6.firebaseapp.com",
+  projectId: "studybooks-423b6",
+  storageBucket: "studybooks-423b6.firebasestorage.app",
+  messagingSenderId: "542433930176",
+  appId: "1:542433930176:web:b9cb2970fc6370d8a27625"
+};
+
+if (!firebase.apps.length) {
+firebase.initializeApp(firebaseConfig);
+}
