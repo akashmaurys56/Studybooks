4b331c45d2fc8d4554ec27d098f59b252d3f5622
akashmaurys56 <mauryaumesh195@gmail.com>
2026-03-04 19:04:28 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,7 +1,10 @@
 var firebaseConfig = {
-apiKey: "YOUR_KEY",
-authDomain: "YOUR_DOMAIN",
-projectId: "YOUR_PROJECT"
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+projectId: "YOUR_PROJECT",
+storageBucket: "YOUR_PROJECT.appspot.com",
+messagingSenderId: "XXXX",
+appId: "XXXX"
 };
 
 if (!firebase.apps.length) {
