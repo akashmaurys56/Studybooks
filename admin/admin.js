8fb1c8d072b54184253bb4a5c6268bfd99249d65
akashmaurys56 <mauryaumesh195@gmail.com>
2026-03-05 16:27:52 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -75,3 +75,99 @@ alert("Section Added");
 
 document.getElementById("sectionPopup").style.display="flex";
 document.getElementById("sectionPopup").style.display="none";
+
+function addClass(){
+
+document.getElementById("classPopup").style.display="flex";
+
+loadSections("classSectionSelect");
+
+}
+
+function closeClassPopup(){
+
+document.getElementById("classPopup").style.display="none";
+
+}
+
+function saveClass(){
+
+let section=document.getElementById("classSectionSelect").value;
+let name=document.getElementById("classInput").value;
+
+db.collection("classes").add({
+section:section,
+name:name
+});
+
+closeClassPopup();
+
+alert("Class Added");
+
+}
+
+function addSubject(){
+
+document.getElementById("subjectPopup").style.display="flex";
+
+loadSections("subjectSectionSelect");
+
+}
+
+function closeSubjectPopup(){
+
+document.getElementById("subjectPopup").style.display="none";
+
+}
+
+function saveSubject(){
+
+let section=document.getElementById("subjectSectionSelect").value;
+let className=document.getElementById("subjectClassSelect").value;
+let name=document.getElementById("subjectInput").value;
+
+db.collection("subjects").add({
+section:section,
+class:className,
+name:name
+});
+
+closeSubjectPopup();
+
+alert("Subject Added");
+
+}
+
+function addChapter(){
+
+document.getElementById("chapterPopup").style.display="flex";
+
+loadSections("chapterSectionSelect");
+
+}
+
+function closeChapterPopup(){
+
+document.getElementById("chapterPopup").style.display="none";
+
+}
+
+function saveChapter(){
+
+let section=document.getElementById("chapterSectionSelect").value;
+let className=document.getElementById("chapterClassSelect").value;
+let subject=document.getElementById("chapterSubjectSelect").value;
+let name=document.getElementById("chapterInput").value;
+
+db.collection("chapters").add({
+section:section,
+class:className,
+subject:subject,
+name:name
+});
+
+closeChapterPopup();
+
+alert("Chapter Added");
+
+}
