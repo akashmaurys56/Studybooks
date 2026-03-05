ecfff859a36e604f93f5b0992d8239794d9046e7
akashmaurys56 <mauryaumesh195@gmail.com>
2026-03-05 15:59:33 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,14 +1,8 @@
 var db = firebase.firestore();
 
 window.onload = function(){
-
-loadSections("classSection");
-loadSections("subjectSection");
-loadSections("chapterSection");
-
+loadSections();
 };
-
-/* SECTION */
 
 function addSection(){
 
@@ -21,69 +15,17 @@ return;
 
 db.collection("sections").add({
 name:name
-});
+}).then(function(){
 
-alert("Section Added");
+alert("Section added");
+
+loadSections();
+
+});
 
 }
 
-/* CLASS */
-
-function addClass(){
-
-let section=document.getElementById("classSection").value;
-let name=document.getElementById("className").value;
-
-db.collection("classes").add({
-section:section,
-name:name
-});
-
-alert("Class Added");
-
-}
-
-/* SUBJECT */
-
-function addSubject(){
-
-let section=document.getElementById("subjectSection").value;
-let className=document.getElementById("subjectClass").value;
-let name=document.getElementById("subjectName").value;
-
-db.collection("subjects").add({
-section:section,
-class:className,
-name:name
-});
-
-alert("Subject Added");
-
-}
-
-/* CHAPTER */
-
-function addChapter(){
-
-let section=document.getElementById("chapterSection").value;
-let className=document.getElementById("chapterClass").value;
-let subject=document.getElementById("chapterSubject").value;
-let name=document.getElementById("chapterName").value;
-
-db.collection("chapters").add({
-section:section,
-class:className,
-subject:subject,
-name:name
-});
-
-alert("Chapter Added");
-
-}
-
-/* LOAD SECTIONS */
-
-function loadSections(selectId){
+function loadSections(){
 
 db.collection("sections").get().then(function(snap){
 
@@ -96,7 +38,28 @@ ${doc.data().name}
 
 </option>`;});
 
-document.getElementById(selectId).innerHTML=html;
+document.getElementById("classSection").innerHTML=html;
+
+});
+
+}
+
+function addClass(){
+
+let section=document.getElementById("classSection").value;
+let name=document.getElementById("className").value;
+
+if(!section || !name){
+alert("Fill all fields");
+return;
+}
+
+db.collection("classes").add({
+section:section,
+name:name
+}).then(function(){
+
+alert("Class added");
 
 });
 
